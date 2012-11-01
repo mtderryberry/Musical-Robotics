6d3139c2cada9031ebbd8f718815dc4d897b47e9
Rayce Stipanovich <ecstipan@rcstipanovich.res.wpi.net>
2012-11-01 19:02:39 -0400

    Added base library

---
+++ MusicalRobots.h
@@ -0,0 +1,14 @@
+//
+//  MusicalRobots.h
+//  
+//
+//  Created by Rayce Stipanovich on 11/1/12.
+//
+//
+
+#ifndef ____MusicalRobots__
+#define ____MusicalRobots__
+
+#include <iostream>
+
+#endif /* defined(____MusicalRobots__) */
