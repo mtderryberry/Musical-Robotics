8692147ebed73bcab5aaaf6b6e7d9f6dcafdf25b
Mike Derryberry <mderryberryt@gmail.com>
2012-11-11 21:26:32 -0500

    updated motor libraries to have velocity control

--- MR_Motor.cpp
+++ MR_Motor.cpp
@@ -1,9 +1,31 @@
 //
 //  MR_Motor.cpp
-//  
+//
 //
 //  Created by Rayce Stipanovich on 11/1/12.
 //
 //
 
 #include "MR_Motor.h"
+
+void setup() {
+    pinMode(MR_MOTOR_PWM_PIN, INPUT);
+    pinMode(MR_MOTOR_F_PIN, OUTPUT);
+    pinMode(MR_MOTOR_R_PIN, OUTPUT);
+}
+
+void velocityControl(int velocity) {
+    while(noteOn == true) {
+        analogWrite(MR_MOTOR_F_PIN, velocity);
+    }
+    analogWrite(MR_MOTOR_F_PIN, 0);
+
+}
+
+void setNoteBooleanTrue() {
+    noteOn = true;
+}
+
+void setNoteBooleanFalse() {
+    noteOn = false;
+}
--- MR_Motor.h
+++ MR_Motor.h
@@ -1,6 +1,6 @@
 //
 //  MR_Motor.h
-//  
+//
 //
 //  Created by Rayce Stipanovich on 11/1/12.
 //
@@ -9,11 +9,14 @@
 #ifndef ____MR_Motor__
 #define ____MR_Motor__
 
+
 #include "Arduino.h"
 #include "MR_IO.h"
 
 #define MR_MOTOR_PWM_PIN 8
 #define MR_MOTOR_F_PIN 9
 #define MR_MOTOR_R_PIN 11
+
+#define noteOn true
 
 #endif /* defined(____MR_Motor__) */
